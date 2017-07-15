e5ab10e8a4eef536cc6f5f9df1d3067e9343cd1e
timzheng0804 <timzheng0804@gmail.com>
2017-07-15 16:09:28 -0400

    Remove Duplicates from Sorted Array

---
+++ Remove Duplicates from Sorted Array.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return nums.size();
+        int pos = 1;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) {
+                nums[pos++] = nums[i + 1];
+            }
+        }
+        return pos;
+    }
+};
