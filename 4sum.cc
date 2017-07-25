50474e986618c68a2b0da92fa732b23ebcdf284a
timzheng0804 <timzheng0804@gmail.com>
2017-07-24 20:20:58 -0400

    modify

--- 4sum.cc
+++ 4sum.cc
@@ -1,26 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (head == NULL) return head;
-        ListNode* acc = head;
-        vector<ListNode*> listNodeCollector;
-        while (acc) {
-            listNodeCollector.push_back(acc);
-            acc = acc->next;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            int difference = target - nums[i];
+            for (int j = i + 1; j < nums.size(); ++j) {
+                int front = j + 1;
+                int end = nums.size() - 1;
+                while (front < end) {
+                    int threeSum = nums[j] + nums[front] + nums[end];
+                    if (threeSum < difference) ++front;
+                    else if (threeSum > difference) --end;
+                    else {
+                        vector<int> sum = { nums[i], nums[j], nums[front], nums[end]};
+                        ans.emplace_back(sum);
+                        while (front < end && nums[front] == sum[2]) front++;
+                        while (front < end && nums[end] == sum[3]) end--;
+                    }
+                }
+                while (j < nums.size() - 1 && nums[j] == nums[j + 1]) ++j;
+            }
+            while (i < nums.size() - 1 && nums[i] == nums[i + 1]) ++i;
         }
-        if (n == listNodeCollector.size()) {
-            return head->next;
-        }
-        int len = listNodeCollector.size();
-        listNodeCollector[len - n - 1]->next = listNodeCollector[len - n]->next;
-        return head;
+        return ans;
     }
 };
