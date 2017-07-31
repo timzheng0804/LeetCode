dc5c813ee0d4340e92115cb9f54ceb066fac51da
timzheng0804 <timzheng0804@gmail.com>
2017-07-31 12:18:51 -0400

    modify

--- Maximum Subarray.cc
+++ Maximum Subarray.cc
@@ -3,12 +3,11 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxSum = nums[0];
         int cur = nums[0];
-        int beginIndex = 0;
         for (int i = 1; i < nums.size(); ++i) {
             cur += nums[i];
             cur = nums[i] > cur ? nums[i] : cur;
             maxSum = cur > maxSum ? cur : maxSum;
         }
-        return maxSum > cur ? maxSum : cur;
+        return maxSum;
     }
 };
