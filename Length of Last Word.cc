398a5edd58e7e1edfc27c5b7bba273e6264d04d4
timzheng0804 <timzheng0804@gmail.com>
2017-07-31 11:43:35 -0400

    Length of Last Word

---
+++ Length of Last Word.cc
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0, tail = s.size() - 1;
+        while (tail >= 0 && s[tail] == ' ') --tail;
+        while (tail >= 0 && s[tail--] != ' ') ++len;
+        return len;
+    }
+};
