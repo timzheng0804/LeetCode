bdef321be03879ce958f09040278c709efd51904
timzheng0804 <timzheng0804@gmail.com>
2017-08-18 21:43:46 -0400

    Binary Tree Level Order Traversal

---
+++ Binary Tree Level Order Traversal.cc
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findLength(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(findLength(root->left), findLength(root->right));
+    }
+    
+    void addLevelNodes(int level, vector<vector<int>>& nodes, TreeNode* root) {
+        if (!root) return;
+        nodes[level].push_back(root->val);
+        ++level;
+        addLevelNodes(level, nodes, root->left);
+        addLevelNodes(level, nodes, root->right);
+    }
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        int length = findLength(root);
+        vector<vector<int>> nodes (length, vector<int>());
+        addLevelNodes(0, nodes,root);
+        return nodes;
+    }
+};
