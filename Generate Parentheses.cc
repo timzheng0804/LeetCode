70b4af6c75841f6b13a83dda63ff7ff379d041b5
timzheng0804 <timzheng0804@gmail.com>
2017-09-03 16:44:06 -0400

    new sol for generate paren

--- Generate Parentheses.cc
+++ Generate Parentheses.cc
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void generateAllPattern(vector<string>& ans) {
-        vector<string> acc;
-        for (int i = 0; i < ans.size(); ++i) {
-            string brackets = ans[i];
-            for (int j = 0; j < brackets.length(); ++j) {
-                if (brackets[j] == '(') {
-                    string s = brackets.substr(0, j + 1) + 
-                        "()" + brackets.substr(j + 1);
-                    acc.emplace_back(s);
-                }
-            }
+    void backTrack(vector<string>& sol, string& s, int left, int right, int n) {
+        if (right == n) {
+            sol.push_back(s);
+            return;
         }
-        ans.swap(acc);
+        
+        if (left < n) {
+            s += "(";
+            backTrack(sol, s, left + 1, right, n);
+            s.pop_back();
+        }
+        if (right < left) {
+            s += ")";
+            backTrack(sol, s, left, right + 1, n);
+            s.pop_back();
+        }
     }
+    
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
+        vector<string> sol;
         string s = "";
-        for (int i = 0; i < n; ++i) {
-            generateAllPattern(ans);
-            s = s + "()";
-            ans.emplace_back(s);
-        }
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
-        return ans;
+        backTrack(sol, s, 0, 0, n);
+        return sol;
     }
 };
