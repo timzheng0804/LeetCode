e751883442d62fedc783c7649ed30259e1efe2b0
timzheng0804 <timzheng0804@gmail.com>
2017-09-06 15:10:16 -0400

    Sort Colors

---
+++ Sort Colors.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0, white = 0, blue = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == 0) ++red;
+            else if (nums[i] == 1) ++white;
+            else ++blue;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (red) { 
+                nums[i] = 0;
+                --red;
+            }
+            else if (white) { 
+                nums[i] = 1;
+                --white;
+            }
+            else nums[i] = 2;
+        }
+    }
+};
