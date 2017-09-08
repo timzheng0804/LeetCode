51871783eaacb48aadc9e75838590ffafaab3b82
timzheng0804 <timzheng0804@gmail.com>
2017-09-07 21:24:16 -0400

    Search a 2D Matrix II

---
+++ Search a 2D Matrix II.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0) return false;
+        int row = 0, col = matrix[0].size() - 1;
+        while (row < matrix.size() && col >= 0) {
+            if (target == matrix[row][col]) return true;
+            else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                ++row;
+            }
+        }
+        return false;
+    }
+};
